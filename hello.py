a4766169fc28130393249f68e4f3f49b03b65f3a
alikulma <alisakul@metropolia.fi>
2024-09-26 10:08:26 +0300

    aloitus

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello")
+# muokkaa tietokantaa
+
+# tervehtii käyttäjää
+
+# kysyy käyttäjänimen
+
+# kysyy lentokokemuksen tason
+
+# arpoo lentokentät
+
+# kysyy lennon suunnan ja pituuden
