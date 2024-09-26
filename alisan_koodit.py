ad5736148cb2d4526997a1f8230edca074b94ba7
alikulma <alisakul@metropolia.fi>
2024-09-26 12:06:31 +0300

    aloitus

--- alisan_koodit.py
+++ alisan_koodit.py
@@ -1,11 +1,12 @@
-# muokkaa tietokantaa
+print("Tervetuloa Lentosuunnistuspeliin!")
 
-# tervehtii käyttäjää
+nimi = input("Syötä käyttäjänimesi: ")
 
-# kysyy käyttäjänimen
+print(f"Hei {nimi}! Tehtävänäsi on suunnistaa maailmalla lentokoneellasi käyttäen lentokenttiä rasteina.")
+print(f"Ensin syötät lentokokemuksesi määrän, jonka jälkeen annamme sinulle viisi lentokenttää kierrettäväksi.")
+print(f"Tavoitteenasi on lentää kuluttaen mahdollisimman vähän lentokilometrejä.")
 
-# kysyy lentokokemuksen tason
+kokemus = input("Syötä lentokokemuksesi taso (1: alle 1 vuosi, 2: 1-10 vuotta, 3: yli 10 vuotta): ")
 
-# arpoo lentokentät
-
-# kysyy lennon suunnan ja pituuden
+print("Nyt syötä, mihin pääilmansuuntaan haluat lentää, ja kuinka monta kilometriä. Esimerkki: 100 km länteen")
+lento = input("Lentosuunta: ")
